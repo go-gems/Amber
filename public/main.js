98d577f964be355e664d9913444e06ecb41d02d2
Benjamin Duchadeuil <benjamin.duchadeuil@gmail.com>
2021-01-24 10:08:15 +0100

    Finish link creation interface

--- public/main.js
+++ public/main.js
@@ -1,5 +1,10 @@
+let confirmOpen = false
+
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#shorten-button").onclick = () => {
+        if (confirmOpen) {
+            return
+        }
         let url
         try {
             url = new URL(document.querySelector("#url-input").value)
@@ -15,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 data.json()
                     .then(json => {
                         copyToClipboard(json.url)
-                        alert(json.url)
+                        showConfirmBox(json.url)
                     })
                     .catch(() => {
                         showError("An unknown error as occured :(")
@@ -59,4 +64,42 @@ function copyToClipboard(text) {
             document.body.removeChild(textarea);
         }
     }
+}
+
+function showConfirmBox(url) {
+    confirmOpen = true
+
+    const html = `
+    <div id="confirm-message" class="absolute inset-0 shadow-lg rounded-2xl p-4 bg-white dark:bg-gray-800 w-80 h-80 z-10 m-auto">
+        <div class="w-full h-full text-center">
+            <div class="flex h-full flex-col justify-between">
+                <svg class="h-12 w-12 mt-4 m-auto text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+                    </path>
+                </svg>
+                <p class="text-gray-600 dark:text-gray-100 text-md py-2 px-6">
+                    There you go !
+                    <br/>
+                    <span class="text-gray-800 dark:text-white font-bold">
+                        <a href="${url}">${url}</a>
+                    </span>
+                    <br/>
+                    The link has been pasted in you clipboard
+                </p>
+                <div class="flex items-center justify-between gap-4 w-full mt-8">
+                    <button onclick="closeConfirmBox()" type="button" class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                        Close
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>`
+
+    document.body.insertAdjacentHTML("beforeend", html)
+
+}
+
+function closeConfirmBox() {
+    document.querySelector("#confirm-message").remove()
+    confirmOpen = false
 }
